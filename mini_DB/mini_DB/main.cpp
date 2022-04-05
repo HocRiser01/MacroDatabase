9440bdbdf43193047415cc5f5cad9ef11e8b722c
HocRiser01 <hocriser@126.com>
2022-04-05 10:24:26 +0800

    first version

---
+++ mini_DB/mini_DB/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mini_DB
+//
+//  Created by 胡剑铮 on 2022/4/5.
+//  Copyright © 2022 胡剑铮. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
